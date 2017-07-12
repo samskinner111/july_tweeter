f67d5ce64d80db7bd7c1887cb13d7202b3c7d26b
samskinnerphd <samskinnerphd@gmail.com>
2017-07-11 23:04:18 -0400

    remove json from tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -22,10 +22,8 @@ class TweetsController < ApplicationController
     respond_to do |format|
       if @tweet.save
         format.html { redirect_to tweets_url, notice: 'Tweet was successfully created.' }
-        format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -34,10 +32,8 @@ class TweetsController < ApplicationController
     respond_to do |format|
       if @tweet.update(tweet_params)
         format.html { redirect_to tweets_url, notice: 'Tweet was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tweet }
       else
         format.html { render :edit }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -46,7 +42,6 @@ class TweetsController < ApplicationController
     @tweet.destroy
     respond_to do |format|
       format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 

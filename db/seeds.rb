fd8b4b756fc50400021de58f77d5efc962d0b40e
samskinnerphd <samskinnerphd@gmail.com>
2017-07-19 17:30:53 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,10 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#   
+#   
 # users = User.create([{email: 'lazlo@example.com', password: 'password', username: 'kiss'}, 
 #                      {email: 'mildred@example.com', password: 'password', username: 'hunkapillar'},
 #                      {email: 'cricket@example.com', password: 'password', username: 'cricket'},
